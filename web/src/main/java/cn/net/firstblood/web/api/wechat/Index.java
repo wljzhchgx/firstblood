73143515faab2989240b84ce80c25839a2c4187f
wljzhchgx <wljzhchgx@sina.com>
2017-04-26 16:55:12 +0800

    tijiao

--- web/src/main/java/cn/net/firstblood/web/api/wechat/Index.java
+++ web/src/main/java/cn/net/firstblood/web/api/wechat/Index.java
@@ -6,6 +6,8 @@ package cn.net.firstblood.web.api.wechat;
 import java.io.IOException;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.servlet.ServletInputStream;
 import javax.servlet.http.HttpServletRequest;
@@ -20,7 +22,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import cn.net.firstblood.biz.cmd.CmdFacade;
 import cn.net.firstblood.biz.model.InputMessage;
 import cn.net.firstblood.biz.model.OutputMessage;
-import cn.net.firstblood.framework.enums.CmdType;
+import cn.net.firstblood.dal.dao.FrequencyDao;
+import cn.net.firstblood.dal.enums.FrequencyBizType;
 import cn.net.firstblood.framework.util.LoggerUtil;
 import cn.net.firstblood.framework.util.SerializeXmlUtil;
 
@@ -32,8 +35,16 @@ import com.thoughtworks.xstream.XStream;
  */
 @Controller("wechatIndex")
 public class Index {
-//	@Autowired
+	private static final Set<String> WHITE_USER = new HashSet<String>();
+	static{
+		WHITE_USER.add("oIPI5xFeEpiExdCzI65hHfCZxIjg");
+	}
+	
+	@Autowired
 	private CmdFacade	cmdFacade;
+	
+	@Autowired
+	private FrequencyDao	frequencyDao;
 	
 	@RequestMapping(value="/api/wechat/index.htm",method = RequestMethod.GET)
     public String executeToken(HttpServletRequest request,ModelMap model) {
@@ -74,18 +85,80 @@ public class Index {
         System.out.println("消息创建时间：" + inputMsg.getCreateTime() + new Date(createTime * 1000l));  
         System.out.println("消息内容：" + inputMsg.getContent());  
         System.out.println("消息Id：" + inputMsg.getMsgId());  
-
+        
+        if(inputMsg.getContent().startsWith("6")){
+        	String result =  getNews(custermname,servername);
+        	LoggerUtil.COMMON.info(result);
+        	model.put("result", result);
+            return "/api/wechat/message";
+        }
+        
+        if(inputMsg.getContent().startsWith("7")){
+        	String result =  getText(custermname,servername,String.valueOf(frequencyDao.addOneTimeByUkKey(FrequencyBizType.EMAIL_TOTAL_DAY, null)));
+        	LoggerUtil.COMMON.info(result);
+        	model.put("result", result);
+            return "/api/wechat/message";
+        }
+        
         StringBuffer str = new StringBuffer();  
         str.append("<xml>");  
         str.append("<ToUserName><![CDATA[" + custermname + "]]></ToUserName>");  
         str.append("<FromUserName><![CDATA[" + servername + "]]></FromUserName>");  
         str.append("<CreateTime>" + returnTime + "</CreateTime>");  
         str.append("<MsgType><![CDATA[" + msgType + "]]></MsgType>");  
-        str.append("<Content><![CDATA["+cmdFacade.exeCmd(CmdType.getByKey(inputMsg.getContent()))+"]]></Content>");  
+        String resultStr = null;
+        if(WHITE_USER.contains(inputMsg.getFromUserName())){
+        	resultStr = cmdFacade.exeCmd(inputMsg.getContent());
+        }else{
+        	resultStr = "非法用户!";
+        }
+        str.append("<Content><![CDATA["+resultStr+"]]></Content>");  
         str.append("</xml>");  
         LoggerUtil.COMMON.info(str.toString());
         //response.getWriter().write(str.toString());
         model.put("result", str.toString());
         return "/api/wechat/message";
+	}
+	
+	private String getText(String custermname,String servername,String content){
+		StringBuffer str = new StringBuffer();  
+        str.append("<xml>");  
+        str.append("<ToUserName><![CDATA[" + custermname + "]]></ToUserName>");  
+        str.append("<FromUserName><![CDATA[" + servername + "]]></FromUserName>");  
+        str.append("<CreateTime>" +(Calendar.getInstance().getTimeInMillis() / 1000)+ "</CreateTime>");  
+        str.append("<MsgType><![CDATA[text]]></MsgType>");  
+        str.append("<Content><![CDATA["+content+"]]></Content>");  
+        str.append("</xml>");  
+        return str.toString();
+	}
+	
+	private String getNews(String custermname,String servername){
+		StringBuffer str = new StringBuffer();  
+		str.append("<xml>");
+		str.append("<ToUserName><![CDATA["+custermname+"]]></ToUserName>");
+		str.append("<FromUserName><![CDATA["+servername+"]]></FromUserName>");
+		str.append("<CreateTime>"+(Calendar.getInstance().getTimeInMillis() / 1000)+"</CreateTime>");
+		str.append("<MsgType><![CDATA[news]]></MsgType>");
+		str.append("<ArticleCount>2</ArticleCount>");
+		str.append("<Articles>");
+		str.append("<item>");
+		str.append("<Title><![CDATA[我是牛逼的]]></Title> ");
+		str.append("<Description><![CDATA[我最厉害 无人能敌]]></Description>");
+		str.append("<PicUrl><![CDATA[http://112.74.197.75:8888/img/check_notify.png]]></PicUrl>");
+		str.append("<Url><![CDATA[http://112.74.197.75/zcb/config.htm]]></Url>");
+		str.append("</item>");
+		str.append("<item>");
+		str.append("<Title><![CDATA[title]]></Title>");
+		str.append("<Description><![CDATA[description]]></Description>");
+		str.append("<PicUrl><![CDATA[http://112.74.197.75:8888/img/check_notify.png]]></PicUrl>");
+		str.append("<Url><![CDATA[http://112.74.197.75/zcb/config.htm]]></Url>");
+		str.append("</item>");
+		str.append("</Articles>");
+		str.append("</xml>");
+		return str.toString();
+	}
+	
+	public static void main(String args[]){
+		System.out.println(new Index().getNews("12","212"));
 	}
 }
